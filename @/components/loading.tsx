e8daa01b876d6a0cb9c8a583287796332654a951
Peter Williams <peterkennethwilliams@gmail.com>
2024-09-27 23:25:09 +0100

    Loading Page
    Page to show before data is fully retrieved added.

---
+++ @/components/loading.tsx
@@ -0,0 +1,43 @@
+import Sidebar from "@/components/Sidebar";
+
+import { type NextPage } from "next";
+import { type SVGProps } from "react";
+
+const Loading: NextPage = () => {
+  return (
+    <>
+      <Sidebar>
+        <div className="flex h-screen flex-col items-center justify-center">
+          <Spinner />
+        </div>
+      </Sidebar>
+    </>
+  );
+};
+
+export default Loading;
+
+function Spinner(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+      >
+        <animateTransform
+          attributeName="transform"
+          dur="0.75s"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 12 12;360 12 12"
+        />
+      </path>
+    </svg>
+  );
+}
